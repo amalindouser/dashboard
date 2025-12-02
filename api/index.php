dc87d1594b377940ab545641ae51ec0374297bed
Bagus Viki Amalindo <vikiamalindo7@gmail.com>
2025-12-02 23:32:34 +0700

    deploy vercel

--- api/index.php
+++ api/index.php
@@ -10,11 +10,8 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require __DIR__ . '/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
