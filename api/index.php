29b6ccdf6800c758bd41a30458d8e0a2ce9d4de5
Bagus Viki Amalindo <vikiamalindo7@gmail.com>
2025-12-03 00:40:19 +0700

    update vercel

--- api/index.php
+++ api/index.php
@@ -5,21 +5,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Maintenance mode
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Composer autoload
 require __DIR__.'/../vendor/autoload.php';
 
-/** @var Application $app */
 $app = require __DIR__ . '/../bootstrap/app.php';
 
 $request = Request::capture();
-
 $response = $app->handleRequest($request);
 
 $response->send();
-
 $app->terminate($request, $response);
